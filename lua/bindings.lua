10924175c8e5fa5cc9dbe9e38262e732a5731e2b
Marcos Roque <devmarcosdanielr@gmail.com>
2025-06-14 23:46:18 -0300

    feat: add lua ffi initial

---
+++ lua/bindings.lua
@@ -0,0 +1,33 @@
+local ffi = require("ffi")
+
+ffi.cdef([[
+  void add_route(const char* method, const char* path, const char* response);
+  void lua_start_server(void);
+]])
+
+local lib = ffi.load("../rust/target/release/librust.dylib")
+
+local Ludi = {}
+Ludi.__index = Ludi
+
+function Ludi.new()
+	return setmetatable({ routes = {} }, Ludi)
+end
+
+function Ludi:get(path, response)
+	table.insert(self.routes, { method = "GET", path = path, response = response })
+end
+
+function Ludi:post(path, response)
+	table.insert(self.routes, { method = "POST", path = path, response = response })
+end
+
+function Ludi:listen()
+	for _, route in ipairs(self.routes) do
+		lib.add_route(route.method, route.path, route.response)
+	end
+
+	lib.lua_start_server()
+end
+
+return Ludi
--- lua/init.lua
+++ lua/init.lua
@@ -1,1 +1,7 @@
-print("Hello, world!")
+local Ludi = require("bindings")
+
+local app = Ludi.new()
+
+app:get("/hello", "Hello, World!")
+
+app:listen()
