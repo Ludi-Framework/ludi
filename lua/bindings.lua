9428878f2d05cc10dc8227fe7f19bdc86a737d47
Azbito <azbitomsg@proton.me>
2025-06-22 21:57:30 -0300

    feat: options in http methods

--- lua/bindings.lua
+++ lua/bindings.lua
@@ -9,8 +9,8 @@ local os_name = jit.os:lower()
 
 local lib_map = {
     windows = "../rust/target/release/rust.dll",
-    linux   = "../rust/target/release/librust.so",
-    osx     = "../rust/target/release/librust.dylib",
+    linux = "../rust/target/release/librust.so",
+    osx = "../rust/target/release/librust.dylib"
 }
 
 local lib_path = lib_map[os_name]
@@ -20,24 +20,50 @@ local lib = ffi.load(lib_path)
 local Ludi = {}
 Ludi.__index = Ludi
 
-function Ludi.new()
-	return setmetatable({ routes = {} }, Ludi)
+function Ludi.new() return setmetatable({routes = {}}, Ludi) end
+local function makeMethod(method)
+    return function(self, path, ...)
+        local args = {...}
+        if #args == 1 then
+            self:addRoute(method, path, nil, args[1])
+        elseif #args == 2 then
+            self:addRoute(method, path, args[1], args[2])
+        else
+            error(
+                ("Expected: %s(path, response) or %s(path, options, response)"):format(
+                    method:lower(), method:lower()))
+        end
+    end
 end
 
-function Ludi:get(path, response)
-	table.insert(self.routes, { method = "GET", path = path, response = response })
+function Ludi:addRoute(method, path, options, response)
+    if type(response) == "function" then
+        local result = response()
+        assert(type(result) == "string",
+               "Response function must return a string")
+        response = result
+    end
+
+    table.insert(self.routes, {
+        method = method,
+        path = path,
+        options = options,
+        response = response
+    })
 end
 
-function Ludi:post(path, response)
-	table.insert(self.routes, { method = "POST", path = path, response = response })
-end
+Ludi.get = makeMethod("GET")
+Ludi.post = makeMethod("POST")
+Ludi.put = makeMethod("PUT")
+Ludi.delete = makeMethod("DELETE")
+-- Ludi.patch = makeMethod("PATCH")
 
 function Ludi:listen(port)
-	for _, route in ipairs(self.routes) do
-		lib.add_route(route.method, route.path, route.response)
-	end
+    for _, route in ipairs(self.routes) do
+        lib.add_route(route.method, route.path, route.response)
+    end
 
-	lib.start_server(port or 3000)
+    lib.start_server(port or 3000)
 end
 
 return Ludi
--- lua/init.lua
+++ lua/init.lua
@@ -2,6 +2,11 @@ local Ludi = require("bindings")
 
 local app = Ludi.new()
 
-app:get("/hello", "hello")
+app:get("/hello", "Hello World")
+app:get("/secure", {auth = true, cache = false}, "Secure Content")
+app:post("/submit", function() return "Form submitted" end)
+app:put("/item/1", {auth = true}, "Item updated")
+app:delete("/item/1", "Item deleted")
+-- app:patch("/item/1", {partial = true}, "Item partially updated")
 
 app:listen(4000)
