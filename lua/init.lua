f8fcc001029d2f0036341c401435ee743f54f24c
Azbito <azbitomsg@proton.me>
2025-06-25 15:17:31 -0300

    [LUA] - feat: example

--- lua/init.lua
+++ lua/init.lua
@@ -2,12 +2,10 @@ local Ludi = require("bindings")
 
 local server = Ludi.new()
 
-server:get("/hello",
-           function(req, res) res:status(202):json({message = "Hello"}) end)
-
-server:post("/echo", function(req, res)
-    res:status(200)
-    res:send(req.body or "Sem corpo")
+server:get("/hello", function(req, res)
+    res:status(202):json({message = "Hello from lua"})
 end)
+
+server:post("/echo", function(req, res) res:status(200):send(req.body) end)
 
 server:listen(3000)
